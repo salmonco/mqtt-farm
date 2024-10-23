faa839fa38e67b4fbce73eff1c6fc458b8514f8f
salmonco <xkqjsslsek80@naver.com>
2024-10-23 22:22:50 +0900

    Chore: 중첩 라우터 설정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,23 +1,13 @@
 // import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "App";
 import "./App.css";
-import FarmListPage from "@pages/FarmList";
-import FarmPage from "@pages/Farm";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <FarmListPage />,
-  },
-  {
-    path: "/farm",
-    element: <FarmPage />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <RouterProvider router={router} />
-  // </React.StrictMode>
+const root = document.getElementById("root") as HTMLElement;
+const element = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
+createRoot(root).render(element);
